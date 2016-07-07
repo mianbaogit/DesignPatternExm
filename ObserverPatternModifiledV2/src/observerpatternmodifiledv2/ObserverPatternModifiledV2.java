74f67b974c27c3682020f9ffa0df86acf237066d
X1 carbon <X1 carbon@Lenovo-PC>
2016-07-07 21:47:44 +0800

    test

---
+++ ObserverPatternModifiledV2/src/observerpatternmodifiledv2/ObserverPatternModifiledV2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package observerpatternmodifiledv2;
+
+/**
+ *
+ * @author X1 carbon
+ */
+public class ObserverPatternModifiledV2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
