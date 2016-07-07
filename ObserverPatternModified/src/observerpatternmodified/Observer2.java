0f5729afcef93bcd7c77345b5fab71b43c972003
X1 carbon <X1 carbon@Lenovo-PC>
2016-07-07 21:16:34 +0800

    test

---
+++ ObserverPatternModified/src/observerpatternmodified/Observer2.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package observerpatternmodified;
+
+/**
+ *
+ * @author cyangmin
+ */
+public class Observer2 implements IObserver{
+
+    @Override
+    public void update(int i) {
+        System.out.println("Observer2: myValue in Subject is now: "+i);
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ ObserverPatternModified/src/observerpatternmodified/ObserverPatternModified.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package observerpatternmodified;
+
+/**
+ *
+ * @author X1 carbon
+ */
+public class ObserverPatternModified {
+
+    /**
+     * @param args the command line arguments
+     */
+     public static void main(String[] args) {
+        System.out.println("*** Modified Observer Pattern Demo***\n");
+        Subject sub = new Subject();
+        Observer1 ob1 = new Observer1();
+        Observer2 ob2 = new Observer2();
+        sub.register(ob1);
+        sub.register(ob2);
+        sub.setMyValue(5);
+        System.out.println();
+        sub.setMyValue(25);
+        System.out.println();
+//unregister ob1 only
+        sub.unregister(ob1);
+//Now only ob2 will observe the change
+        sub.setMyValue(100);
+    }
+    
+}
